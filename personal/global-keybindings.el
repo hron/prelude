c8cddfaa0a31231ff65c761f2e77009f9f163492
Aleksei Gusev <aleksei.gusev@gmail.com>
2015-01-24 10:24:28 +0300

    Fix helm global key binding

--- personal/global-keybindings.el
+++ personal/global-keybindings.el
@@ -7,7 +7,7 @@
 (global-set-key (kbd "C-<backspace>") 'backward-kill-word)
 (global-set-key [remap other-window] 'other-window)
 (global-set-key (kbd "C-c g") 'ag)
-(global-set-key (kbd "S-SPC") 'helm-prelude)
+(global-set-key (kbd "S-SPC") 'helm-projectile)
 (global-set-key [f6] 'toggle-truncate-lines)
 (global-set-key [f1] 'yari-helm)
 (global-set-key [f2] 'winner-undo)

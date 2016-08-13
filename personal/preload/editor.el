c8e1ce68e722f800344e1d312821e5a1752b5050
Aleksei Gusev <aleksei.gusev@gmail.com>
2016-08-13 17:05:27 +0300

    Set solarized-light theme

---
+++ personal/preload/editor.el
@@ -0,0 +1,3 @@
+;(prelude-require-package 'color-theme-solarized)
+;(prelude-require-package 'color-theme-solarized)
+(setq prelude-theme 'solarized-light)
--- personal/ui.el
+++ personal/ui.el
@@ -5,10 +5,8 @@
                       (abbreviate-file-name (buffer-file-name))
                     "%b"))))
 
-(disable-theme 'zenburn)
-(prelude-require-package 'solarized-theme)
-(load-theme 'solarized-light t)
-
 (setq-default left-fringe-width 7)
 
 (setq use-dialog-box nil)
+
+(prelude-require-package 'color-theme-solarized)

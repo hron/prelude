fba2b85cdb057bc936cc5f00266709703cb3af8f
Aleksei Gusev <aleksei.gusev@freska.fi>
2019-03-12 09:48:44 +0200

    Add C-S-t keybinding

--- personal/projectile.el
+++ personal/projectile.el
@@ -3,4 +3,5 @@
 (add-hook 'projectile-mode-hook 
           (lambda () 
             (local-set-key (kbd "C-c t") 'projectile-toggle-between-implementation-and-test)
+            (local-set-key (kbd "C-S-t") 'projectile-toggle-between-implementation-and-test)
             (local-set-key (kbd "C-n") 'projectile-find-file-dwim)))

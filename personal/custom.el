148f004917533caa9e1d393efeb0bc6e722fe798
Aleksei Gusev <aleksei.gusev@freska.fi>
2019-03-15 11:16:04 +0200

    Customer `mode-line-inactive`

--- personal/custom.el
+++ personal/custom.el
@@ -99,4 +99,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#3F3F3F" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
- '(mode-line ((t (:background "#2B2B2B" :foreground "#8FB28F" :box (:line-width -1 :style released-button) :weight normal :height 0.95 :width condensed)))))
+ '(mode-line ((t (:background "#2B2B2B" :foreground "#8FB28F" :box (:line-width -1 :style released-button) :weight normal :height 0.95 :width condensed))))
+ '(mode-line-inactive ((t (:background "#383838" :foreground "#5F7F5F" :box (:line-width -1 :style released-button) :height 0.95)))))

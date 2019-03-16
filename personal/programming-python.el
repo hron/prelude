6236c0dd92b6ba5a99235573e82cb9a5081bee15
Aleksei Gusev <aleksei.gusev@gmail.com>
2019-03-16 20:53:03 +0200

    Setup python mode

--- personal/programming-python.el
+++ personal/programming-python.el
@@ -1,8 +1,16 @@
 (prelude-require-package 'use-package)
-(prelude-require-package 'ipython-shell-send)
 
 (setq python-shell-interpreter "ipython"
       python-shell-interpreter-args "--simple-prompt -i")
 
+(prelude-require-package 'ipython-shell-send)
 (use-package ipython-shell-send
   :bind (:map python-mode-map ([remap python-shell-send-region] . ipython-shell-send-region)))
+
+(use-package anaconda-mode
+  :bind (:map anaconda-mode-map
+              ("C-i" . anaconda-mode-show-doc)
+              ("C-b" . anaconda-mode-find-definitions)))
+
+(prelude-require-package 'company-anaconda)
+(use-package company-anaconda)

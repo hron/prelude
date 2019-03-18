9cd809ad7a44c37edf52789b2109a60fbf347850
Aleksei Gusev <aleksei.gusev@gmail.com>
2019-03-18 20:21:48 +0200

    Fix ESS mode

--- personal/programming-ess.el
+++ personal/programming-ess.el
@@ -1,6 +1,6 @@
 (prelude-require-package 'use-package)
 (prelude-require-package 'ess)
 
-(use-package ess
+(use-package ess-mode
   :bind (:map ess-mode-map
               ("M-e" . ess-eval-region)))

56f4cae7153b678172309c42061cf3da9c74bab4
Aleksei Gusev <aleksei.gusev@freska.fi>
2019-03-18 11:42:04 +0200

    Enable prelude-whitespace

--- personal/prelude.el
+++ personal/prelude.el
@@ -1,5 +1,6 @@
 (setq prelude-guru nil)
 ;(prelude-global-mode -1)
+(setq prelude-whitespace t)
 
 (setq dired-listing-switches "-ahl --group-directories-first")
 (add-hook 'dired-mode-hook (lambda () (dired-omit-mode)))
